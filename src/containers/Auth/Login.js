0965ecfec99d9417b191536e07cf6511e0c37463
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-06-20 22:52:02 +0700

    hoc state onClick Event and show hide password

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -5,10 +5,39 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
+import { HighlightSpanKind } from 'typescript';
 
 class Login extends Component {
 	constructor(props) {
-		super(props); this.btnLogin = React.createRef();
+		super(props);
+		this.state = {
+			username: '',
+			password: '',
+			isShowPass: false,
+		}
+	}
+
+	handleOnChangeInput = (event) => {
+		console.log(event)
+		this.setState({
+			username: event.target.value
+		});
+	}
+
+	handleOnChangePass = (event) => {
+		this.setState({
+			password: event.target.value
+		});
+	}
+
+	handleLogin = () => {
+	
+	}
+
+	handleShowHidePass = () => {
+		this.setState({
+			isShowPass: !this.state.isShowPass
+		})
 	}
 
 	render() {
@@ -19,14 +48,31 @@ class Login extends Component {
 						<div className="col-12 text-login">Login</div>
 						<div className="col-12 form-group login-input">
 							<label>Username</label>
-							<input className="form-control" type="text" placeholder="Enter your username"/>
+							<input
+								className="form-control"
+								type="text"
+								placeholder="Enter your username"
+								value={this.state.username}
+								onChange={(event) => this.handleOnChangeInput(event)}
+							/>
 						</div>
 						<div className="col-12 form-group login-input">
 							<label>Password</label>
-							<input className="form-control" type="password" placeholder="Enter your password"/>
+							<div className="custom-password">
+								<input
+									className="form-control"
+									type={this.state.isShowPass ? 'text' : 'password'}
+									placeholder="Enter your password"
+									value={this.state.password}
+									onChange={(event) => this.handleOnChangePass(event)}
+								/>
+								<span
+									onClick={()=>{this.handleShowHidePass()}}
+								><i className={this.state.isShowPass ? "far fa-eye-slash" : "far fa-eye"}></i></span>
+							</div>
 						</div>
 						<div className="col-12">
-							<button className="btn-login">Login</button>
+							<button className="btn-login" onClick={()=>{this.handleLogin()}}>Login</button>
 						</div>
 						<div className="col-12">
 							<span className="forgot-pass">Forgot your password?</span>
@@ -40,23 +86,23 @@ class Login extends Component {
 						</div>
 					</div>
 				</div>
-      </div>
-				)
-    }
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = state => {
-    return {
-					language: state.app.language
-    };
+	return {
+		language: state.app.language
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-					navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
-    };
+	return {
+		navigate: (path) => dispatch(push(path)),
+		adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+		adminLoginFail: () => dispatch(actions.adminLoginFail()),
+	};
 };
 
-				export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
