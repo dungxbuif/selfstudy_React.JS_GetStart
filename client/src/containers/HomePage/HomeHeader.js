20ad9520cf1a56e5549348d8f4712adf6429174f
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-07-15 15:34:08 +0700

    #46 Khai bao file dich

--- client/src/containers/HomePage/HomeHeader.js
+++ client/src/containers/HomePage/HomeHeader.js
@@ -2,61 +2,138 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
+import { FormattedMessage } from 'react-intl';
 
 class HomeHeader extends Component {
    render() {
       return (
-         <div className="home-header-container">
-            <div className="home-header-content container ">
-               <div className="row">
-                  <div className="left-content">
-                     <i className="fas fa-bars"></i>
-                     <div className="header-logo"></div>
+         <>
+            <div className="home-header-container">
+               <div className="home-header-content container ">
+                  <div className="row">
+                     <div className="left-content">
+                        <i className="fas fa-bars"></i>
+                        <div className="header-logo"></div>
+                     </div>
+                     <div className="center-content">
+                        <div className="child-content">
+                           <div className="child-title">
+                              <b>
+                                 <FormattedMessage id="home-header.speciality" />
+                              </b>
+                           </div>
+                           <div className="child-descript">
+                              <FormattedMessage id="home-header.search-doctor" />
+                           </div>
+                        </div>
+
+                        <div className="child-content">
+                           <div className="child-title">
+                              <b>
+                                 <FormattedMessage id="home-header.health-facility" />
+                              </b>
+                           </div>
+                           <div className="child-descript">
+                              <FormattedMessage id="home-header.choose-hospital" />
+                           </div>
+                        </div>
+
+                        <div className="child-content">
+                           <div className="child-title">
+                              <b>
+                                 <FormattedMessage id="home-header.medical-combo" />
+                              </b>
+                           </div>
+                           <div className="child-descript">
+                              <FormattedMessage id="home-header.health-check" />
+                           </div>
+                        </div>
+
+                        <div className="child-content">
+                           <div className="child-title">
+                              <b>
+                                 <FormattedMessage id="home-header.doctor" />
+                              </b>
+                           </div>
+                           <div className="child-descript">
+                              <FormattedMessage id="home-header.choose-doc" />
+                           </div>
+                        </div>
+                     </div>
                   </div>
-                  <div className="center-content">
-                     <div className="child-content">
-                        <div className="child-title">
-                           <b>Chuyên khoa</b>
-                        </div>
-                        <div className="child-descript">
-                           Tìm bác sỹ theo chuyên khoa
-                        </div>
+                  <div className="right-content">
+                     <div className="row">
+                        <i className="fas fa-question-circle"></i>
+                        <span>Hỗ trợ</span>
+                        <div className="lang-vi">VN</div>
+                        <div className="lang-en">EN</div>
                      </div>
-
-                     <div className="child-content">
-                        <div className="child-title">
-                           <b>Cơ sở y tế</b>
-                        </div>
-                        <div className="child-descript">
-                           Chọn bệnh viện phòng khám
-                        </div>
-                     </div>
-
-                     <div className="child-content">
-                        <div className="child-title">
-                           <b>Gói khám</b>
-                        </div>
-                        <div className="child-descript">
-                           Khám sức khỏe tổng quát
-                        </div>
-                     </div>
-
-                     <div className="child-content">
-                        <div className="child-title">
-                           <b>Bác sỹ</b>
-                        </div>
-                        <div className="child-descript">Chọn bác sỹ giỏi</div>
-                     </div>
-                  </div>
-               </div>
-               <div className="right-content">
-                  <div>
-                     <i class="fas fa-question-circle"></i>
-                     <span>Hỗ trợ</span>
                   </div>
                </div>
             </div>
-         </div>
+            <div className="home-banner">
+               <div className="banner-search">
+                  <div className="banner-search-container">
+                     <h1>
+                        Nền tảng y tế <br></br>
+                        <b>chăm sóc sức khỏe toàn diện</b>
+                     </h1>
+                     <div className="timkiem-form" id="timkiem_form">
+                        <div className="timkiem-onhap">
+                           <i className="fas fa-search"></i>
+                           <input
+                              id="timkiem_chung"
+                              type="search"
+                              placeholder="Tìm gói khám"
+                           />
+                        </div>
+                     </div>
+                     <div className="tai-ungdung">
+                        <img
+                           alt="Tải ứng dụng BookingCare trên Android"
+                           src="https://bookingcare.vn//assets/icon/google-play-badge.svg"
+                           width="108"
+                           height="32"
+                        />
+                        <img
+                           alt="Tải ứng dụng BookingCare trên iOS"
+                           src="https://bookingcare.vn//assets/icon/app-store-badge-black.svg"
+                           width="108"
+                           height="32"
+                        />
+                     </div>
+                  </div>
+               </div>
+               <div className="luachon">
+                  <ul>
+                     <li>
+                        <div className="dichvu kham-chuyenkhoa luoi-tai"></div>
+                        Khám Chuyên khoa
+                     </li>
+                     <li>
+                        <div className="dichvu kham-tuxa luoi-tai"></div>
+                        Khám từ xa
+                     </li>
+                     <li>
+                        <div className="dichvu kham-tongquat luoi-tai"></div>
+                        Khám tổng quát
+                     </li>
+                     <li>
+                        <div className="dichvu dichvu-xetnghiem luoi-tai"></div>
+                        Xét nghiệm y học
+                     </li>
+                     <li>
+                        <div className="dichvu dichvu-xetnghiem luoi-tai"></div>
+                        Sức khỏe tinh thần
+                     </li>
+                     <li>
+                        <div className="dichvu kham-nhakhoa luoi-tai"></div>
+                        Khám nha khoa
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </>
       );
    }
 }
