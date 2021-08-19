4c631fdea85944be38562383f99a872fb5545cec
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-08-19 10:48:50 +0700

    #72 24:00

--- client/src/containers/System/Doctor/MangeShedule.js
+++ client/src/containers/System/Doctor/MangeShedule.js
@@ -1,14 +1,101 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './ManageSchedule.scss';
+import * as actions from '../../../store/actions';
+import Select from 'react-select';
+import { getDoctorDetailInfo } from '../../../services/userService';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../../utils';
+import DatePiker from '../../../components/Input/DatePicker';
 class ManageSchedule extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         contentMarkdown: '',
+         contentHTML: '',
+         selectedDoctor: '',
+         description: '',
+         hasExisted: false,
+         allDoctors: [],
+         listDoctors: [],
+      };
+   }
+
+   componentDidMount() {
+      this.props.fetchALllDoctors();
+   }
+
+   componentDidUpdate(prevProps, prevStates, snapShot) {
+      if (prevProps.allDoctors !== this.props.allDoctors) {
+         this.convertDataToSelect();
+      }
+      if (prevProps.language !== this.props.language) {
+         this.convertDataToSelect();
+      }
+   }
+
+   convertDataToSelect = () => {
+      let arr = [...this.props.allDoctors];
+      let { language } = this.props;
+
+      if (this.props.allDoctors && this.props.allDoctors.length > 0) {
+         this.setState({
+            listDoctors: arr.map((item) => ({
+               label:
+                  language === LANGUAGES.VI
+                     ? `${item.lastName} ${item.firstName}`
+                     : `${item.firstName} ${item.lastName}`,
+               value: item.id,
+            })),
+         });
+      }
+      return [];
+   };
+
+   handleChange = async (selectedDoctor) => {
+      this.setState({ selectedDoctor });
+
+      let res = await getDoctorDetailInfo(selectedDoctor.value);
+      // if (res && res.code === 0 && res.data && res.data.markDownData) {
+      //    let markdown = res.data.markDownData;
+      //    this.setState({
+      //       contentMarkdown: markdown.contentMarkdown,
+      //       description: markdown.description,
+      //       hasExisted: true,
+      //    });
+      // } else {
+      //    this.setState({
+      //       contentMarkdown: '',
+      //       description: '',
+      //       hasExisted: false,
+      //    });
+      // }
+      console.log(res);
+   };
+
    render() {
-      const { isLoggedIn } = this.props;
       return (
          <div className="manage-schedule-container">
-            <div className="m-s-title">
+            <div className="m-s-title my-3">
                <FormattedMessage id="manage-shedule.title" />
+            </div>
+            <div className="container">
+               <div className="row">
+                  <div className="col-6 form-group">
+                     <label>Chọn bác sỹ</label>
+                     <Select
+                        value={this.state.selectedDoctor}
+                        onChange={this.handleChange}
+                        options={this.state.listDoctors}
+                     />
+                  </div>
+                  <div className="col-6 form-group">
+                     <label>Chọn ngày</label>
+                     import {(LANGUAGES, CRUD_ACTIONS)} from '../../../utils';
+                  </div>
+                  <div className="col-12 pick-hour-container"></div>
+                  <button className="btn btn-primary mt-3">Lưu thông tin</button>
+               </div>
             </div>
          </div>
       );
@@ -17,13 +104,15 @@ class ManageSchedule extends Component {
 
 const mapStateToProps = (state) => {
    return {
-      systemMenuPath: state.app.systemMenuPath,
-      isLoggedIn: state.user.isLoggedIn,
+      language: state.app.language,
+      allDoctors: state.admin.allDoctors,
    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-   return {};
+   return {
+      fetchALllDoctors: () => dispatch(actions.fetchALllDoctors()),
+   };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule);
