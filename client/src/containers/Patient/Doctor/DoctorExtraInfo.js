a84411ebb7c2e34b705fea3cd8ad2fd478aa3643
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-08-23 00:03:08 +0700

    #80

--- client/src/containers/Patient/Doctor/DoctorExtraInfo.js
+++ client/src/containers/Patient/Doctor/DoctorExtraInfo.js
@@ -5,10 +5,18 @@ import './DoctorExtraInfo.scss';
 class DoctorExtraInfo extends Component {
    constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+         isShowInfo: false,
+      };
    }
 
    componentDidMount() {}
+
+   togglePriceDetail() {
+      this.setState({
+         isShowInfo: !this.state.isShowInfo,
+      });
+   }
 
    componentDidUpdate(prevProprs, prevStates, snapshot) {
       if (this.props.language !== prevProprs.language) {
@@ -16,7 +24,37 @@ class DoctorExtraInfo extends Component {
    }
 
    render() {
-      return <div className="doctor-extra-info-container">Test extra doctor info </div>;
+      return (
+         <div className="doctor-extra-info-container p-3">
+            <div className="content-top">
+               <div className="address-title">Đặt lịch khám</div>
+               <div className="clinic-name">Tên phòng khám</div>
+               <div className="address">Địa chỉ</div>
+            </div>
+            <div className="content-down">
+               <div className="price-title">
+                  Giá khám:{' '}
+                  {!this.state.isShowInfo ? (
+                     <span className="toggle" onClick={() => this.togglePriceDetail()}>
+                        Xem chi tiết
+                     </span>
+                  ) : null}
+               </div>
+               {this.state.isShowInfo ? (
+                  <>
+                     <div className="price-detail">
+                        <div>250.000 đ</div>
+                        <div>Được ưu tiên khám trước fdafdsfds dfdf dfdfwre s sdf s sfd sd sd</div>
+                        <div>Được ưu tiên khám trước fdafdsfds dfdf dfdfwre s sdf s sfd sd sd</div>
+                     </div>
+                     <div className="toggle" onClick={() => this.togglePriceDetail()}>
+                        Ẩn bảng giágit ad
+                     </div>
+                  </>
+               ) : null}
+            </div>
+         </div>
+      );
    }
 }
 
